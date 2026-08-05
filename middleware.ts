18bb79ef54c50a86c46fa78477ce64c45fd13d34
techtig <techtig9@gmail.com>
2026-08-05 03:29:07 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -37,7 +37,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request: { headers: request.headers } });
           cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
